fed5adb5422505fe5c2545e57c37fa4efe896422
ninjaherodev <ninjaherodev@gmail.com>
2024-06-03 11:29:16 -0500

    ejemplo de funciones para string

---
+++ string-avanzado/main.py
@@ -0,0 +1,26 @@
+text = "Ella sabe programar en python"
+
+print('javascript' in text)
+print('python' in text)
+
+size = len('amor    '.strip())
+print(size)
+
+print(text)
+print(text.upper())
+
+print(text)
+print(text.lower())
+print(text.count('a'))
+
+print(text.swapcase())
+
+print(text.startswith('Ella'))
+print(text.endswith('Rust'))
+print(text.replace('python','Go'))
+
+text_2 = 'este es un titulo'
+print(text_2)
+print(text_2.capitalize())
+print(text_2.title())
+print(text_2.isdigit())
