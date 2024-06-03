601dcb0cbee5db6d8be02a816b4cdf0f76b3b134
ninjaherodev <ninjaherodev@gmail.com>
2024-06-03 11:58:56 -0500

    indexing

---
+++ indexing/code.py
@@ -0,0 +1,19 @@
+text = "ella sabe python"
+print(text[0])
+print(text[1])
+print(text[-2])
+
+# Slicing
+
+print(text[0:6])
+print(text[10:16])
+
+print(text[0:10])
+print(text[:10])
+print(text[0:])
+
+print(text[10:16:1])
+print(text[::-1])
+print(text[10:16:2])
+
+print(text[::2])
